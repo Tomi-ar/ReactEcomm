45fe2a29e5fd5a0d81e2849b2b9cacc51e56f3fc
Tomi Ar <tomas.arrillagab@gmail.com>
2021-07-24 12:31:32 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( < div > Clase instalación y configuración de React < ul >
+    <
+    li > Uno < /li>  <li> Dos </li > < li > Tres < /li>  </ul > < /div>,
+    document.getElementById('root'));
